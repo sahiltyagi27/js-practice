6d975c95fc29199b8d9fa3308ef532c8f111592a
Sahil Tyagi <sahiltyagi1997@gmail.com>
2021-04-04 12:31:03 +0530

    merge 04/04/21

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-function mergeSort(A, p, q, r) {
-    let i, j, k;
-    let L = [];
-    let R = []
-    let n1 = q - p + 1;
-    let n2 = r - q - 1;
+function merge(A, lb, m, ub) {
+    let n1 = m - lb + 1;
+    let n2 = ub - m;
+    let L = [n1];
+    let R = [n2];
+    let i, j;
     for (i = 0; i < n1; i++) {
-        L[i] = A[p + i];
+        L[i] = A[lb + i];
     }
     for (j = 0; j < n2; j++) {
-        R[j] = A[q + j + 1];
+        R[j] = A[m + 1 + j];
     }
-    L.push(Infinity);
-    console.log(L)
-    R.push(Infinity);
-    console.log(R);
-    i = 0;
-    j = 0;
-    for (k = p; k < r; k++) {
+
+    let k = lb;
+    while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             A[k] = L[i];
             i++;
@@ -24,8 +20,32 @@ function mergeSort(A, p, q, r) {
             A[k] = R[j];
             j++;
         }
+        k++;
+    }
+
+    while (i < n1) {
+        A[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        A[k] = R[j];
+        j++;
+        k++;
     }
     console.log(A);
 }
 
-mergeSort([2, 4, 5, 1, 2, 8], 0, 2, 6)
+function mergeSort(A, lb, ub) {
+    let m;
+    if (ub == 1) {
+        return;
+    }
+    m = (lb + ub) / 2
+    mergeSort(A, lb, m);
+    mergeSort(A, m + 1, ub)
+    merge(A, lb, m, ub);
+
+}
+
+mergeSort([5], 0, 1);
